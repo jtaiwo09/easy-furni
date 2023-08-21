070d4913f5a67ff4d256e37e54e94ea17a840862
jtaiwo09 <taiwokelvin@gmail.com>
2023-08-21 21:27:22 +0100

    fixed cors error...

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(express.static("public"));
 app.use(
   cors({
     credentials: true,
-    origin: "https://jtk-store.vercel.app",
+    origin: ["https://jtk-store.vercel.app", "http://localhost:3000"],
   })
 );
 app.use(express.json({ limit: "50mb" }));
