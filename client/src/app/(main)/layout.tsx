5315fbda22fc0b74f0bbc9d3909890f25c34e012
jtaiwo09 <taiwokelvin@gmail.com>
2023-08-22 07:53:38 +0100

    removed global error page

--- client/src/app/(main)/layout.tsx
+++ client/src/app/(main)/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({
 }) {
   const token = cookies().get("token")?.value ?? null;
   const sellerToken = cookies().get("seller_token")?.value ?? null;
+
+  console.log("TOKEN", token);
   return (
     <html lang="en">
       <body className={montserrat.className}>
