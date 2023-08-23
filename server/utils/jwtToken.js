cf18d9e1396f877b38a5bba194273eba474977b3
jtaiwo09 <taiwokelvin@gmail.com>
2023-08-23 18:16:18 +0100

    fixing token bug

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -3,15 +3,15 @@ const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
 
   // Options for cookies
-  const options = {
-    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    sameSite: "None",
-    secure: true,
-    path: "/",
-  };
+  // const options = {
+  //   expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+  //   httpOnly: true,
+  //   sameSite: "None",
+  //   secure: true,
+  //   path: "/",
+  // };
 
-  res.status(statusCode).cookie("token", token, options).json({
+  res.status(statusCode).json({
     success: true,
     user,
     token,
