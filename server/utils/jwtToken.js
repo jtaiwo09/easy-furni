03734fef1f10e43330f0df42e09a9f0960096e25
jtaiwo09 <taiwokelvin@gmail.com>
2023-08-23 08:33:23 +0100

    commit

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -6,10 +6,9 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: "none",
+    sameSite: "None",
     secure: true,
     path: "/",
-    domain: ".vercel.app",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
